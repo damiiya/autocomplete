26712eda3cbe7adbc26e4653d8a0f7e455616bd8
damiiya <ddam2lee@gmail.com>
2022-10-29 01:28:30 +0900

    📂 docs. 프로젝트 기본 파일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import SearchHead from "./component/searchHead";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header>
+          <h1>SEARCH HERE</h1>
+        </Header>
+        <SearchHead />
+        <input></input>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 900px;
+  margin: 200px auto;
+`;
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 40px auto;
+  h1 {
+    font-size: 48px;
+    font-weight: 700;
+    color: #3f9bfc;
+  }
+`;
---
+++ src/component/SearchHead.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchHead = () => {
+  return <input></input>;
+};
+
+export default SearchHead;

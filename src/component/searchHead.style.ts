9755e5cf8b4bc307964a9aebf4ed0e07de630797
damiiya <ddam2lee@gmail.com>
2022-10-31 08:22:25 +0900

    📝 feat. 인풋 & 드롭 컴포넌트 스타일 파일 작성

--- src/component/searchHead.style.ts
+++ src/component/searchHead.style.ts
@@ -1,18 +1,14 @@
 import styled from "styled-components";
+import { DropPropsType } from "../type";
 
 export const Container = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-export const InputContainer = styled.div`
-  width: 700px;
-  display: flex;
   flex-direction: column;
+  justify-content: center;
 `;
 export const Input = styled.input`
-  width: 700px;
+  width: 100%;
   height: 60px;
   padding: 20px;
   box-sizing: border-box;
@@ -26,18 +22,65 @@ export const Input = styled.input`
     color: #cdd3de;
   }
 `;
-export const Button = styled.button`
-  width: 150px;
-  height: 60px;
-  font-size: 24px;
-  font-weight: 500;
-  color: #fff;
-  background-color: #3f9bfc;
+export const DropContainer = styled.div<DropPropsType>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: ${(props) =>
+    props.maxHeight &&
+    props.maxHeight * (props.height ? props.height : 98) + "px"};
+  margin-top: 30px;
+  box-sizing: border-box;
   border: none;
-  border-radius: 10px;
+  box-shadow: 0px 2px 30px #e2e7eb;
+  overflow-y: ${(props) => (props.isScroll ? "scroll" : "hidden")};
+  }
+`;
+export const DropList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+export const DropItem = styled.li<DropPropsType>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: ${(props) => (props.height ? props.height + "px" : "98px")};
+  padding: 0 24px;
+  background-color: ${(props) =>
+    props.isFocus === true ? props.backColor : "#fff"};
   :hover {
     cursor: pointer;
-    transition: 0.5s;
-    background-color: #187ae0;
+    background-color: ${(props) =>
+      props.backColor ? props.backColor : "#fff"};
   }
+`;
+export const MainInfo = styled.div<DropPropsType>`
+  display: flex;
+  flex-direction: column;
+  width: ${(props) => (props.width ? props.width : "90%")};
+`;
+export const MainTxt = styled.p<DropPropsType>`
+  font-size: ${(props) =>
+    props.fontSize ? props.fontSize * 2 + "px" : "14px"};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight * 100 : 400)};
+  color: ${(props) => (props.color ? props.color : "#000")};
+  margin: 0 0 12px 0;
+`;
+export const SubTxt = styled.span<DropPropsType>`
+  font-size: ${(props) =>
+    props.fontSize ? props.fontSize * 2 + "px" : "14px"};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight * 100 : 400)};
+  color: ${(props) => (props.color ? props.color : "#000")};
+  line-height: ${(props) =>
+    props.lineHeight ? props.lineHeight * 2 + "px" : "14px"};
+`;
+export const SubInfo = styled.p<DropPropsType>`
+  text-align: right;
+  font-size: ${(props) =>
+    props.fontSize ? props.fontSize * 2 + "px" : "14px"};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight * 100 : 400)};
+  width: ${(props) => (props.width ? props.width : "10%")};
+  color: ${(props) => (props.color ? props.color : "#000")};
 `;

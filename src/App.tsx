5dc8a5623636e3048a6a900d8048c495b25449e3
damiiya <ddam2lee@gmail.com>
2022-10-31 08:20:28 +0900

    📝 feat. 페이지 라우터 처리 & 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import "./App.css";
 import styled from "styled-components";
-import SearchHead from "./component/SearchHead";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { EnExample } from "./routes/EnExample";
+import { KnExample } from "./routes/KnExample";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <Container>
+        <ButtonWrapper>
+          <Button onClick={() => navigate("/enExample")}>영어</Button>
+          <Button onClick={() => navigate("/knExample")}>한국어</Button>
+        </ButtonWrapper>
         <Header>
           <h1>SEARCH HERE</h1>
         </Header>
-        <SearchHead />
+        <Routes>
+          <Route path="/enExample" element={<EnExample />}></Route>
+          <Route path="/knExample" element={<KnExample />}></Route>
+        </Routes>
       </Container>
     </div>
   );
@@ -18,8 +28,9 @@ function App() {
 export default App;
 
 const Container = styled.div`
-  width: 900px;
-  margin: 200px auto;
+  margin: 0 auto;
+  margin-top: 40px;
+  width: 700px;
 `;
 const Header = styled.div`
   width: 100%;
@@ -30,5 +41,22 @@ const Header = styled.div`
     font-size: 48px;
     font-weight: 700;
     color: #3f9bfc;
+  }
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const Button = styled.button`
+  width: 70px;
+  color: #83a6db;
+  background-color: #f4efeb;
+  padding: 5px;
+  margin: 5px;
+  border: none;
+  border-radius: 30px;
+  font-weight: 700;
+  :hover {
+    cursor: pointer;
   }
 `;
